3238b5687d076c146f83a1925e7beed99793c2f9
CheckMate Solutions Inc <kylen@checkmatsolutions.ca>
2016-04-15 16:01:11 -0300

    Merge branch 'master' of https://github.com/Knewcombe/checkmateLaw
    * 'master' of https://github.com/Knewcombe/checkmateLaw:
      Changes made to iOS
      Adding updated functionality for memo recording size.

--- CheckmateLaw/platforms/ios/www/js/app-js/contollers/about_controller.js
+++
@@ -1,8 +0,0 @@
-angular.module("app").controller('AboutController', function($scope, $rootScope) {
-    $rootScope.isHomepage = false;
-    $rootScope.isResizeDiv = false;
-
-        $scope.versionNumber = "1.0";
-        $scope.currentDate = new Date();
-        $scope.copyright = "Copyright " + $scope.year + " Checkmate";
-});
--- CheckmateLaw/platforms/ios/www/js/app-js/directives/boolean_value.js
+++
@@ -1,6 +0,0 @@
-angular.module('app')
-.directive('booleanValue', function() {
-    return function(scope, elm, attr) {
-        attr.$set('value', attr.booleanValue === 'true');
-    };
-});
